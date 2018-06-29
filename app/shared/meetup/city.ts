8b41bbd38d468bb9aa126140b821f0bed20ea0da
Javier Font <javifont@gmail.com>
2018-06-29 22:10:28 +0200

    service & mode (city)

---
+++ app/shared/meetup/city.ts
@@ -0,0 +1,12 @@
+export class City {
+    zip: string;
+    country: string; // es
+    localized_country_name: string; // "España",
+    distance: number; // 4.23875365168966,
+    city: string; // "Alaquàs",
+    "lon": -0.46000000834465027,
+"ranking": 17,
+"id": 1011013,
+"member_count": 1,
+"lat": 39.45000076293945
+}
