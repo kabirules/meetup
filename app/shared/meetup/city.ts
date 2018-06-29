2b3541713cdb4c7d69ca8462f5ec6ab41460c5ef
kabirules <javifont@gmail.com>
2018-06-30 00:09:29 +0200

    City object

--- app/shared/meetup/city.ts
+++ app/shared/meetup/city.ts
@@ -1,12 +1,13 @@
 export class City {
+    
     zip: string;
     country: string; // es
     localized_country_name: string; // "España",
     distance: number; // 4.23875365168966,
     city: string; // "Alaquàs",
-    "lon": -0.46000000834465027,
-"ranking": 17,
-"id": 1011013,
-"member_count": 1,
-"lat": 39.45000076293945
+    lon: number; // -0.46000000834465027,
+    ranking: number; //17,
+    id: number; // 1011013,
+    member_count: number // 1,
+    lat: number; // 39.45000076293945
 }
